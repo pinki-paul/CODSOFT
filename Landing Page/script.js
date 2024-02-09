cb848c48b551f5009b25717895f907f2ceaa43b5
pinki-paul <141720649+pinki-paul@users.noreply.github.com>
2024-02-09 21:31:21 +0530

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,21 @@
+// navbar scrolling
+
+let navbar = document.querySelector(".navbar");
+
+window.onscroll = function(){
+    if (document.documentElement.scrollTop > 20) {
+        navbar.classList.add("header-scrolled");
+    }else{
+        navbar.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide
+ let navLink = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+navLink.forEach((a)=>{
+    a.addEventListener("click", ()=> {
+        navCollapse.classList.remove("show");
+    });
+});
