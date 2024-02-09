b16b345f58ae740f8d6d1bb2f81ebf7f8509eb1d
pinki-paul <141720649+pinki-paul@users.noreply.github.com>
2024-02-09 21:27:58 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,28 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let result = "";
+
+let buttonArray = Array.from(buttons);
+
+buttonArray.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML === "=") {
+            result = eval(result);
+            input.value = result;
+        }
+
+        else if (e.target.innerHTML === "AC") {
+            result = "";
+            input.value = result;
+        }
+        else if (e.target.innerHTML === "DE") {
+            result = result.substring(0, result.length - 1);
+            input.value = result;
+        }
+        else {
+            result = result + e.target.innerHTML;
+            input.value = result;
+        }
+    });
+});
